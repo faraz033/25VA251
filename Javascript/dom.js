ca0fcb401ad155402fef3a67eeb7173143dc186b
faraz033 <farazfarooqui033@gmail.com>
2026-03-16 16:22:00 +0530

    Javascript

---
+++ Javascript/dom.js
@@ -0,0 +1,10 @@
+function show(){
+    let name = document.getElementById("myName").ariaValueMax;
+    //document.getElementById("display").innerHTML= name;
+    alert(name);
+}
+
+//dom
+function changeColor(){
+    document.getElementById('text').style.color='red';
+}
